e960b030d4de3394e97b5d43cb387af93d382520
SPmfl <matheo.franco.loaiza@gmail.com>
2025-07-12 02:01:39 -0500

    basic template with lot of changes

---
+++ src/i18n/ui.ts
@@ -0,0 +1,106 @@
+export const languages = {
+  en: 'English',
+  es: 'Español'
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'change-language': 'Change language',
+    meta: {
+      title: "Matheo's Portfolio",
+      description: "Matheo's Portfolio of projects."
+    },
+    '404': {
+      title: 'Page not found',
+      description: 'The page you are looking for does not exist',
+      content: {
+        '1': 'Page not found',
+        '2': 'The page you are looking for does not exist'
+      },
+      back: 'Back to home'
+    },
+    navbar: {
+      home: 'Home',
+      experience: 'Experience',
+      projects: 'Projects',
+      contact: 'Contact'
+    },
+    hero: {
+      words:{
+        '1': 'EAT',
+        '2': 'SLEEP',
+        '3': 'CODE',
+        '4': 'REPEAT' 
+      },
+      salute: 'Hi! I am',
+      description: 'I work as a fullstack developer and I would like to help you.',
+      cta: 'Send me a message!'
+    },
+    experience: {
+      title: 'Experience',
+      present: 'Present',
+      content: {
+        '1': 'Freelancer',
+        '2': 'I have worked on various freelance projects, including web development and software solutions.',
+        '3': 'Fullstack Developer',
+        '4': 'I have experience in both frontend and backend development, working with technologies like  React, Node.js, and more.',
+        '5': 'Freelancer',
+        '6': 'I have worked on various freelance projects, including web development and software solutions.',
+      }
+    },
+    projects: {
+      title: 'Projects',
+    },
+  },
+  es: {
+    'change-language': 'Cambiar idioma',
+    meta: {
+      title: 'El portafolio de Matheo',
+      description: 'Portafolio de projectos para mostrar.'
+    },
+    '404': {
+      title: 'Página no encontrada',
+      description:
+        'La página que buscas no existe o no la he creado aún. Por favor, verifica e inténtalo de nuevo.',
+      content: {
+        '1': 'Página no encontrada',
+        '2': 'La página que buscas no existe o no la he creado aún. Por favor, verifica e inténtalo de nuevo.'
+      },
+      back: 'Volver al inicio'
+    },
+    navbar: {
+      home: 'Inicio',
+      experience: 'Experiencia',
+      projects: 'Proyectos',
+      contact: 'Contacto'
+    },
+    hero: {
+      words:{
+        '1': 'COMER',
+        '2': 'DORMIR',
+        '3': 'PROGRAMAR',
+        '4': 'REPETIR' 
+      },
+      salute: 'Hola! Soy',
+      description: 'Trabajo como fullstack developer y me gustaría ayudarte.',
+      cta: 'Escribeme!'
+    },
+    experience: {
+      title: 'Experiencia',
+      present: 'Presente',
+      content: {
+        '1': 'Freelancer',
+        '2': 'He trabajado en varios proyectos freelance, incluido desarrollo web y soluciones de software.',
+        '3': 'Desarrollador Fullstack',
+        '4': 'He trabajado como desarrollador backend y frontend, usando tecnologias como ReactJS, Node.js y más.',
+        '5': 'Freelancer',
+        '6': 'Comienzo del desarrollo de soluciones web como freelance.',
+      }
+    },
+    projects: {
+      title: 'Proyectos',
+    },
+  }
+}
